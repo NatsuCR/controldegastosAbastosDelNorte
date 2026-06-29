0f51873595be2c71a265882ff43af9e494135c99
Carlos Andrés Jara Villalobos <169032671+NatsuCR@users.noreply.github.com>
2026-06-29 00:45:29 -0600

    Update API tunnel URL

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://pink-ladybug-5.loca.lt/api';
+const API_URL = 'https://dull-seals-hang.loca.lt/api';
 
 type ApiRequest = Omit<RequestInit, 'headers'> & {
   headers?: Record<string, string>;
